0ba9b9f7d497749e6fc40e4e20f0685209053c60
Tanmoydas27 <dast91330@gmail.com>
2024-06-22 14:11:19 +0530

    correction

--- client/server/dbConfig/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({ 
-    cloud_name: process.env.cloud_name, 
-    api_key: process.env.cloud_api_key, 
-    api_secret: process.env.clodu_api_secret_key 
-  });
-
-module.exports = cloudinary;
